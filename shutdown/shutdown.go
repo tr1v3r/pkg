61b3a8157785c0ce57a44b43bd6c7bb307fd65c0
r1v3r <churiver@outlook.com>
2023-05-17 14:29:54 +0800

    feat: add package shutdown

---
+++ shutdown/shutdown.go
@@ -0,0 +1,22 @@
+package shutdown
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+)
+
+var ctx, stop = signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+// Cancel return shutdown signal chan
+func Cancel() <-chan struct{} { return ctx.Done() }
+
+// Cancelled judge if project shutdown
+func Cancelled() bool {
+	select {
+	case <-Cancel():
+		return true
+	default:
+		return false
+	}
+}
