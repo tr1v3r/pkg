9cb6086a9214ffc045c4fc966ade91d0ca61bcd2
r1v3r <churiver@outlook.com>
2023-05-31 16:05:42 +0800

    feat: update log func with ctx and level

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pkg
+
+DIY packages
--- log/log.go
+++ log/log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"context"
 	"io"
 
 	"github.com/sirupsen/logrus"
@@ -8,6 +9,16 @@ import (
 
 type Level = logrus.Level
 type Formatter = logrus.Formatter
+
+const (
+	PanicLevel Level = logrus.PanicLevel
+	FatalLevel Level = logrus.FatalLevel
+	ErrorLevel Level = logrus.ErrorLevel
+	WarnLevel  Level = logrus.WarnLevel
+	InfoLevel  Level = logrus.InfoLevel
+	DebugLevel Level = logrus.DebugLevel
+	TraceLevel Level = logrus.TraceLevel
+)
 
 var defaultLogger = logrus.New()
 
@@ -39,8 +50,18 @@ func Trace(format string, args ...interface{}) {
 	defaultLogger.Tracef(format, args...)
 }
 
+// CtxTrace ...
+func CtxTrace(_ context.Context, format string, args ...interface{}) {
+	defaultLogger.Tracef(format, args...)
+}
+
 // Debug ...
 func Debug(format string, args ...interface{}) {
+	defaultLogger.Debugf(format, args...)
+}
+
+// CtxDebug ...
+func CtxDebug(_ context.Context, format string, args ...interface{}) {
 	defaultLogger.Debugf(format, args...)
 }
 
@@ -49,8 +70,18 @@ func Info(format string, args ...interface{}) {
 	defaultLogger.Infof(format, args...)
 }
 
+// CtxInfo ...
+func CtxInfo(_ context.Context, format string, args ...interface{}) {
+	defaultLogger.Infof(format, args...)
+}
+
 // Warn ...
 func Warn(format string, args ...interface{}) {
+	defaultLogger.Warnf(format, args...)
+}
+
+// CtxWarn ...
+func CtxWarn(_ context.Context, format string, args ...interface{}) {
 	defaultLogger.Warnf(format, args...)
 }
 
@@ -59,12 +90,27 @@ func Error(format string, args ...interface{}) {
 	defaultLogger.Errorf(format, args...)
 }
 
+// CtxError ...
+func CtxError(_ context.Context, format string, args ...interface{}) {
+	defaultLogger.Errorf(format, args...)
+}
+
 // Fatal ...
 func Fatal(format string, args ...interface{}) {
 	defaultLogger.Fatalf(format, args...)
 }
 
+// CtxFatal ...
+func CtxFatal(_ context.Context, format string, args ...interface{}) {
+	defaultLogger.Fatalf(format, args...)
+}
+
 // Panic ...
 func Panic(format string, args ...interface{}) {
+	defaultLogger.Panicf(format, args...)
+}
+
+// CtxPanic ...
+func CtxPanic(_ context.Context, format string, args ...interface{}) {
 	defaultLogger.Panicf(format, args...)
 }
