c12845792f4408b1c3777e0b880d63198e680b03
tr1v3r <churiver@outlook.com>
2024-10-15 01:16:13 +0800

    feat: update group attrs

--- rss/opml.go
+++ rss/opml.go
@@ -26,28 +26,28 @@ type Body struct {
 type OutlineArray []*Outline
 
 // Append adds a new outline to the OutlineArray, grouped by the specified groupText and groupTitle.
-func (a OutlineArray) Append(groupText, groupTitle string, o *Outline) OutlineArray {
+func (a OutlineArray) Append(groupTitle string, o *Outline) OutlineArray {
 	if o.Text == "" || o.XMLUrl == "" { // invalid outline
 		return a
 	}
-	if groupText == "" || groupTitle == "" { // group info cannot be empty
+	if groupTitle == "" { // group info cannot be empty
 		return a
 	}
 
 	for _, group := range a {
-		if group.Text == groupText { // group found
+		if group.Title == groupTitle { // group found
 			group.Outlines = append(group.Outlines, o)
 			return a
 		}
 	}
-	return append(a, &Outline{Text: groupText, Title: groupTitle, Outlines: []*Outline{o}})
+	return append(a, &Outline{Title: groupTitle, Outlines: []*Outline{o}})
 }
 
 // Outline defines the structure of an outline element in the OPML document.
 type Outline struct {
 	Type     string     `xml:"type,attr,omitempty"`
-	Text     string     `xml:"text,attr"`
-	Title    string     `xml:"title,attr"`
+	Text     string     `xml:"text,attr,omitempty"`
+	Title    string     `xml:"title,attr,omitempty"`
 	XMLUrl   string     `xml:"xmlUrl,attr,omitempty"`
 	HTMLUrl  string     `xml:"htmlUrl,attr,omitempty"`
 	Outlines []*Outline `xml:"outline,omitempty"` // 用于支持嵌套结构
