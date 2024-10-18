5ce881f3a18fc9bcd41ae9c0b8ad51a99113c1a0
tr1v3r <churiver@outlook.com>
2024-10-19 02:43:20 +0800

    feat(notion): add more filter conditions

--- notion/filter.go
+++ notion/filter.go
@@ -81,13 +81,54 @@ func (cond *FilterCondition) MarshalJSON() ([]byte, error) {
 // FilterSingleCondition filter single condition
 // https://developers.notion.com/reference/post-database-query-filter#the-filter-object
 type FilterSingleCondition struct {
-	Property string          `json:"property"`
-	CheckBox *CheckBoxFilter `json:"checkbox,omitempty"`
-	RichText *RichTextFilter `json:"rich_text,omitempty"`
-	Number   *NumberFilter   `json:"number,omitempty"`
-	Files    *FilesFilter    `json:"files,omitempty"`
+	Property    string             `json:"property"`
+	CheckBox    *CheckBoxFilter    `json:"checkbox,omitempty"`
+	Date        *DateFilter        `json:"date,omitempty"`
+	Files       *FilesFilter       `json:"files,omitempty"`
+	Formula     *FormulaFilter     `json:"formula,omitempty"`
+	MultiSelect *MultiSelectFilter `json:"multi_select,omitempty"`
+	Number      *NumberFilter      `json:"number,omitempty"`
+	People      *PeopleFilter      `json:"people,omitempty"`
+	Relation    *RelationFilter    `json:"relation,omitempty"`
+	RichText    *RichTextFilter    `json:"rich_text,omitempty"`
+	Rollup      *RollupFilter      `json:"rollup,omitempty"`
+	Select      *SelectFilter      `json:"select,omitempty"`
+	Status      *StatusFilter      `json:"status,omitempty"`
+	Timestamp   *TimestampFilter   `json:"timestamp,omitempty"`
+	ID          *IDFilter          `json:"ID,omitempty"`
 
 	Contains string `json:"contains,omitempty"`
+}
+
+// SelectFilter ...
+type SelectFilter struct {
+	Equals       string `json:"equals,omitempty"`
+	DoesNotEqual string `json:"does_not_equal,omitempty"`
+	IsEmpty      bool   `json:"is_empty,omitempty"`
+	IsNotEmpty   bool   `json:"is_not_empty,omitempty"`
+}
+
+type MultiSelectFilter struct {
+	Contains       string `json:"contains,omitempty"`
+	DoesNotContain string `json:"does_not_contain,omitempty"`
+	IsEmpty        bool   `json:"is_empty,omitempty"`
+	IsNotEmpty     bool   `json:"is_not_empty,omitempty"`
+}
+
+// DateFilter ...
+// https://developers.notion.com/reference/post-database-query-filter#date
+type DateFilter struct {
+	After      string    `json:"after,omitempty"`  // ISO_8601 Date
+	Before     string    `json:"before,omitempty"` // ISO_8601 Date
+	Equals     string    `json:"equals,omitempty"` // ISO_8601 Date
+	OnOrAfter  *struct{} `json:"on_or_after,omitempty"`
+	OnOrBefore *struct{} `json:"on_or_before,omitempty"`
+	PastMonth  *struct{} `json:"past_month,omitempty"`
+	PastWeek   *struct{} `json:"past_week,omitempty"`
+	PastYear   *struct{} `json:"past_year,omitempty"`
+	ThisWeek   *struct{} `json:"this_week,omitempty"`
+	IsEmpty    bool      `json:"is_empty,omitempty"`
+	IsNotEmpty bool      `json:"is_not_empty,omitempty"`
 }
 
 // CheckBoxFilter ...
@@ -121,12 +162,63 @@ type NumberFilter struct {
 	IsNotEmpty           bool    `json:"is_not_empty,omitempty"`
 }
 
+// PeopleFilter ...
+type PeopleFilter struct {
+	Contains       string `json:"contains,omitempty"`         // UUIDv4
+	DoesNotContain string `json:"does_not_contain,omitempty"` // UUIDv4
+	IsEmpty        bool   `json:"is_empty,omitempty"`
+	IsNotEmpty     bool   `json:"is_not_empty,omitempty"`
+}
+
+// RelationFilter ...
+type RelationFilter struct {
+	Contains       string `json:"contains,omitempty"`         // UUIDv4
+	DoesNotContain string `json:"does_not_contain,omitempty"` // UUIDv4
+	IsEmpty        bool   `json:"is_empty,omitempty"`
+	IsNotEmpty     bool   `json:"is_not_empty,omitempty"`
+}
+
 // FilesFilter ...
 // doc: https://developers.notion.com/reference/post-database-query-filter#files
 type FilesFilter struct {
 	IsEmpty    bool `json:"is_empty,omitempty"`
 	IsNotEmpty bool `json:"is_not_empty,omitempty"`
 }
+
+// FormulaFilter ...
+type FormulaFilter struct {
+	CheckBox *CheckBoxFilter `json:"checkbox,omitempty"`
+	Date     *DateFilter     `json:"date,omitempty"`
+	Number   *NumberFilter   `json:"number,omitempty"`
+	RichText *RichTextFilter `json:"rich_text,omitempty"`
+}
+
+// RollupFilter ...
+// https://developers.notion.com/reference/post-database-query-filter#rollup
+type RollupFilter struct {
+	Any   map[string]any `json:"any,omitempty"`
+	Every map[string]any `json:"every,omitempty"`
+	None  map[string]any `json:"none,omitempty"`
+}
+
+// StatusFilter ...
+type StatusFilter struct {
+	Equals       string `json:"equals,omitempty"`
+	DoesNotEqual string `json:"does_not_equal,omitempty"`
+	IsEmpty      bool   `json:"is_empty,omitempty"`
+	IsNotEmpty   bool   `json:"is_not_empty,omitempty"`
+}
+
+// TimestampFilter ...
+type TimestampFilter struct {
+	Timestamp      string      `json:"timestamp,omitempty"`
+	CreatedTime    *DateFilter `json:"created_time,omitempty"`
+	LastEditedTime *DateFilter `json:"last_edited_time,omitempty"`
+}
+
+// IDFilter ...
+// https://developers.notion.com/reference/post-database-query-filter#id
+type IDFilter map[any][]NumberFilter
 
 type PropSortCondition struct {
 	Property  string `json:"property"`
